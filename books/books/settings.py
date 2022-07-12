ec5b93dc335941b8fd933dab28d49e3f13499785
riv-ryzhkov <riv.ryzhkov@gmail.com>
2022-07-13 02:16:39 +0300

    good version3

--- books/books/settings.py
+++ books/books/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'debug_toolbar',
+    # 'debug_toolbar',
     'django_extensions',
     'rest_framework',
 
